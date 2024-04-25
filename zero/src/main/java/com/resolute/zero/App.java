52cd91001fe603d09e7ceb0d5a6be1666fb9da81
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-04-25 18:10:47 +0530

    build(app): basic setup
    - created one controller
    - created one model
    - added few properties in yaml file
    - connected db

---
+++ zero/src/main/java/com/resolute/zero/App.java
@@ -0,0 +1,10 @@
+package com.resolute.zero;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class App {
+    @Id
+    private int id;
+}
---
+++ zero/src/main/java/com/resolute/zero/JokerController.java
@@ -0,0 +1,11 @@
+package com.resolute.zero;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JokerController {
+
+    @GetMapping
+    public String hi(){return "bye";}
+}
