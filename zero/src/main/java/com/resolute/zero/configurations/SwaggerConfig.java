7ec68ac81e03e6ded996ba0f5ae2b30493bd7874
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-04-26 18:25:25 +0530

    custom configuration added

---
+++ zero/src/main/java/com/resolute/zero/configurations/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.resolute.zero.configurations;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI employeeManagementOpenAPI() {
+
+        var api = new OpenAPI();
+        api.info(new Info().title("resolve")
+                .license(new License().name("resolve"))
+        );
+        api.components(new Components());
+
+        return api;
+    }
+}
+
