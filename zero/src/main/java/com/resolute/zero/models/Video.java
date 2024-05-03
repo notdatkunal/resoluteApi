f8e0e9af2db8b6f3d3f09e18375c2bbefe5e1824
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-05-03 11:38:06 +0530

    video model added from stash

--- zero/src/main/java/com/resolute/zero/models/Video.java
+++ zero/src/main/java/com/resolute/zero/models/Video.java
@@ -1,4 +1,26 @@
 package com.resolute.zero.models;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.sql.Blob;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Video {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true)
+    private String name;
+
+    private Blob video;
+
+
+
 }
