d683b1c162c706974a249862862cfa59afd8f90f
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-05-07 08:01:01 +0530

    controllers models updated

---
+++ zero/src/main/java/com/resolute/zero/requests/AdminCaseRequest.java
@@ -0,0 +1,46 @@
+package com.resolute.zero.requests;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Date;
+
+@Builder
+@Data
+public class AdminCaseRequest {
+    String state;
+    String zone;
+    String branchName;
+    String customerId;
+    String accountNumber;
+    String creditCardNumber;
+    String customerName;
+    String actualProduct;
+    String flagProductGroup;
+    String natureOfLegalAction;
+    String totalTos;
+    String totalTosInCr;
+    Date noticeDate;
+    Date refLetter;
+    String caseNo;
+    Date socFillingDate;
+    String claimAmountInSOC;
+    Date firstHearingDate;
+    Date lastHearingDate;
+
+    Date stagesOfLastHearingDate;
+    Date nextHearingDate;
+    Date stagesOfNextHearingDate;
+    String caseStatus;
+    String flagForContested;
+    String detailsRemark;
+    Date awardDate;
+    String awardAmount;
+    Date sec17AppFillingDate;
+    Date sec17AppStatus;
+    String courtName;
+    String place;
+    String arbitrator;
+    String lawyerName;
+    String lmName;
+}
