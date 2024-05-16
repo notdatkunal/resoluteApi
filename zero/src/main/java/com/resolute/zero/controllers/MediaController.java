f7b0881538b93c1e112621bc583d45ec25428643
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-05-16 23:20:10 +0530

    authentication added to MediaController

--- zero/src/main/java/com/resolute/zero/controllers/MediaController.java
+++ zero/src/main/java/com/resolute/zero/controllers/MediaController.java
@@ -1,8 +1,10 @@
 package com.resolute.zero.controllers;
 
 import com.resolute.zero.services.MediaService;
+import com.resolute.zero.utilities.ApplicationUtility;
 import com.resolute.zero.utilities.CodeComponent;
 import com.resolute.zero.utilities.MetaDocInfo;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -24,7 +26,8 @@ public class MediaController {
     private MediaService mediaService;
 
     @PostMapping("/media")
-    public ResponseEntity<?> singleUploading(@RequestParam("file") MultipartFile file, @RequestParam String mainType,@RequestParam String subType,@RequestParam Integer caseId) throws IOException {
+    public ResponseEntity<?> singleUploading(HttpSession session, @RequestParam("file") MultipartFile file, @RequestParam String mainType, @RequestParam String subType, @RequestParam Integer caseId) throws IOException {
+        ApplicationUtility.authenticate(session);
         String code = codeComponent.getCode(mainType,subType,caseId);
         String fileName =  mediaService.uploadFile(code,file);
         mediaService.saveDocumentInDB(codeComponent.getMetaDocInfo(code));
@@ -32,7 +35,8 @@ public class MediaController {
     }
 
     @PostMapping("/media/bulk")
-    public ResponseEntity<?> bulkUploading(@RequestParam("files") MultipartFile[] files) throws IOException {
+    public ResponseEntity<?> bulkUploading(HttpSession session,@RequestParam("files") MultipartFile[] files) throws IOException {
+        ApplicationUtility.authenticate(session);
         var fileNamesList = mediaService.uploadFiles(files);
         mediaService.saveDocumentsInDB(codeComponent.getMetaDocsInfo(fileNamesList));
         return ResponseEntity.ok("file uploaded with names "+fileNamesList);
