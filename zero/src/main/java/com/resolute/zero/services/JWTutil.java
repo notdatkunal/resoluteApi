6c388c6a83dfcb2931d69145c0dd037d95aca226
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-05-28 08:38:05 +0530

    token modified

--- zero/src/main/java/com/resolute/zero/services/JWTutil.java
+++ zero/src/main/java/com/resolute/zero/services/JWTutil.java
@@ -8,7 +8,7 @@ import java.util.Date;
 public class JWTutil
 {
 
-    private static final String SECRET = "secret-key";
+    private static final String SECRET = "YourSecretKeyToMyAPIYourSecretKeyToMyAPIYourSecretKeyToMyAPIYourSecretKeyToMyAPIYourSecretKeyToMyAPI";
     private static final long EXP_TIME_IN_10_DAYS = 864_000_000;
     public static  String generateToken(String username){
         return Jwts.builder().setSubject(username).setExpiration(new Date(System.currentTimeMillis()+EXP_TIME_IN_10_DAYS))
